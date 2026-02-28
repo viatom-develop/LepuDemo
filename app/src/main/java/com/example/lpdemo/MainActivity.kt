bd48c651c86bc4b04084576ff292e2dc027b5a1b
caijiahuan <caijiahuan@lepucloud.com>
2026-02-28 16:35:33 +0800

    The O2 series has added encrypted communication

--- app/src/main/java/com/example/lpdemo/MainActivity.kt
+++ app/src/main/java/com/example/lpdemo/MainActivity.kt
@@ -330,7 +330,7 @@ class MainActivity : AppCompatActivity(), BleChangeObserver {
                 // stop scan before connect
                 BleServiceHelper.BleServiceHelper.stopScan()
                 // connect
-                BleServiceHelper.BleServiceHelper.connect(applicationContext, it.model, it.device)
+                BleServiceHelper.BleServiceHelper.connect(applicationContext, it.model, it.device, bluetooth = it)
 
                 deviceModel = it.model
                 deviceName = it.name
